cf218b9f23bd4cb1fe30e15b44dc0d7975f558ff
1-mukesh-1 <51417684+1-mukesh-1@users.noreply.github.com>
2021-05-15 16:48:47 +0530

    leetcode

--- root/cpp1.cpp
+++ root/cpp1.cpp
@@ -1,33 +1,34 @@
-#include<bits/stdc++.h>
-#define endl "\n"
-using namespace std;
 
-void solve(){
-    int n,m;
-    cin>>n>>m;
-    int summ=0;
-    int temp;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            cin>>temp;
-            summ+=temp*(((n-i)*(m-j))*((i+1)*(j+1)));
+
+    bool isSymmetric(TreeNode* root)
+    {
+    vector<int>v;
+    Inorder(root,v);
+    int l;
+    l=v.size()-1;
+    for(int i=0;i<(l/2);i++)
+    {
+        if(v[i]!=v[l-i])
+        {
+            return false;
+            break;
         }
-    }
-    cout<<summ<<endl;
-}
 
-signed main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    int t=1;
-    // cin>>t;
-    while(t--){
-        solve();
+
     }
-    cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl;
-    return 0;
-}
+
+    return true;
+
+    }
+
+    vector<int> Inorder(TreeNode* root,vector<int> &v)
+    {
+         if(root==NULL)
+            return;
+        isSymmetric(root->left);
+        v.push_back(root->val);
+        isSymmetric(root->right);
+
+        return v;
+
+    }

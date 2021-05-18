db67ccfc892f407b0bc70ec617024ad572fb4965
1-mukesh-1 <51417684+1-mukesh-1@users.noreply.github.com>
2021-05-18 11:00:22 +0530

    old snapshot

--- root/cpp1.cpp
+++ root/cpp1.cpp
@@ -1,34 +1,27 @@
+#include<bits/stdc++.h>
+#define int long long
+#define endl "\n"
+using namespace std;
 
+void solve(){
+    int n;
+    cin>>n;
+    cout<<n<<endl;
+    return;
+}
 
-    bool isSymmetric(TreeNode* root)
-    {
-    vector<int>v;
-    Inorder(root,v);
-    int l;
-    l=v.size()-1;
-    for(int i=0;i<(l/2);i++)
-    {
-        if(v[i]!=v[l-i])
-        {
-            return false;
-            break;
-        }
-
-
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
     }
-
-    return true;
-
-    }
-
-    vector<int> Inorder(TreeNode* root,vector<int> &v)
-    {
-         if(root==NULL)
-            return;
-        isSymmetric(root->left);
-        v.push_back(root->val);
-        isSymmetric(root->right);
-
-        return v;
-
-    }
+    cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl;
+    return 0;
+}
